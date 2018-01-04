f8c3cff07dd12e4678cbccfde65fbb17469bfc21
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 15:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ names = ["Abel", "Ari", "Koi"]
 
 function printBadges(array) {
   for (i = 0; i < array.length; i++) {
-    console.log("Welcome " + array[i] + "! You are employee #" + 1 + ".")
+    console.log("Welcome " + array[i] + "! You are employee #" + i + ".")
   }
   return array
 }
